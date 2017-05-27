c915901c4b1807fbfb97ff7ab79fd23c5953b15b
Marc Ferrer <mark.mff@gmail.com>
2017-05-27 19:58:49 +0200

    Leap controls improvement.
    Now it will take into account arm direction vector to
    to return the position.

--- games/cubes-road/js/leap-controls.js
+++ games/cubes-road/js/leap-controls.js
@@ -13,21 +13,31 @@ controller
 })
 .connect();
 
-// base min and max yaws
-let minYaw = -1.1;
-let maxYaw = 1.3;
-
+let maxRightYaw = 0.9;
+let maxLeftYaw = 0.6;
 export function getPosition(leftLimit, rightLimit) {
   const frame = controller.frame();
   if (frame.hands && frame.hands.length > 0) {
     const hand = frame.hands[0];
 
-    const yaw = hand.yaw();
-    // TODO: Check if yaw exceeded min or max yaw and update them if so.
-    if (yaw > 0) {
-      return yaw * rightLimit / maxYaw;
+    const handVector = [hand.direction[0], hand.direction[2]];
+    const armVector = [hand.arm.direction()[0], hand.arm.direction()[2]];
+    const dotProduct = Leap.glMatrix.vec2.dot(handVector, armVector);
+    const lengths = Leap.glMatrix.vec2.len(handVector) * Leap.glMatrix.vec2.len(armVector); // eslint-disable-line
+    const cos = dotProduct / lengths;
+    const yawAngle = Math.acos(cos);
+    if (handVector[0] > armVector[0]) {
+      if (yawAngle > maxRightYaw) {
+        console.log('Updating max yaw angle to the right', yawAngle);
+        maxRightYaw = yawAngle;
+      }
+      return yawAngle * rightLimit / maxRightYaw;
     }else {
-      return yaw * leftLimit / minYaw;
+      if (yawAngle > maxLeftYaw) {
+        console.log('Updating max yaw angle to the left', yawAngle);
+        maxLeftYaw = yawAngle;
+      }
+      return yawAngle * leftLimit / maxLeftYaw;
     }
   }else {
     return null;
